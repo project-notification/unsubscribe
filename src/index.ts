995bb2abf5021ecf33acb8256e1ab4376d16a8eb
leemhoon00 <leemhoon000@gmail.com>
2024-07-20 23:47:03 +0900

    feat: unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 import { Handler, APIGatewayEvent } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DeleteCommand } from '@aws-sdk/lib-dynamodb';
+import { emailRegex } from './regex';
 
 export const handler: Handler = async (event: APIGatewayEvent) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'abc',
-    }),
-  };
+  const email: string = JSON.parse(event.body!).email;
+
+  if (!emailRegex.test(email)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Invalid email' }),
+    };
+  }
+
+  const client = new DynamoDBClient({});
+
+  const command = new DeleteCommand({
+    TableName: 'subscription',
+    Key: {
+      email,
+    },
+  });
+
+  try {
+    await client.send(command);
+    return {
+      statusCode: 204,
+    };
+  } catch (error) {
+    console.error('Fail');
+    console.error(error);
+    return {
+      statusCode: 500,
+    };
+  }
 };
